d6049efe3bfb8a873f491cb49f3f5ca889e5c896
Sebastián Camargo <sebas@slash-studio.com>
2022-07-12 17:06:37 -0500

    Base commit
    This commit starts off the journey of the Mirayoki site :)

---
+++ qr/js/serverSpeeds.js
@@ -0,0 +1,1 @@
+const chosenNumber=Math.floor(101*Math.random()+200);document.getElementById("speed").textContent=`${chosenNumber} queries per second`;
---
+++ qr/js/setUrl.js
@@ -0,0 +1,1 @@
+const urlPool=['https://bit.ly/3c2LTZD','https://bit.ly/3PcoAea','https://bit.ly/3IpHJHg','https://bit.ly/3uB686V'];const chosenURL=urlPool[Math.floor(Math.random()*urlPool.length)];document.getElementById("theLink").href=chosenURL;
